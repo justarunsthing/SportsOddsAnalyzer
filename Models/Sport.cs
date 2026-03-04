8020350832884bbd682294f5e46ce08acc28c95b
arunpun <83875939+arunpun@users.noreply.github.com>
2026-03-04 20:42:50 +0000

    Added Sport model

---
+++ Models/Sport.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsOddsAnalyzer.Models
+{
+    public class Sport
+    {
+        public string? Key { get; set; }
+        public string? Group { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool Active { get; set; }
+        public bool HasOutrights { get; set; }
+    }
+}
