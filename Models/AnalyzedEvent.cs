9f4463bcd276d99a1321044f8ab4413f45e9fdd8
arunpun <83875939+arunpun@users.noreply.github.com>
2026-03-04 20:54:59 +0000

    Added AnalyzedEvent class

---
+++ Models/AnalyzedEvent.cs
@@ -0,0 +1,44 @@
+﻿namespace SportsOddsAnalyzer.Models
+{
+    public class AnalyzedEvent : Event
+    {
+        public BestOddsSummary BestOdds { get; set; } = new();
+        public MarginInfo Margin { get; set; } = new();
+        public ConsensusOdds Consensus { get; set; } = new();
+        public ValueBetsInfo ValueBets { get; set; } = new();
+    }
+
+    public class BestOddsSummary
+    {
+        public OddsInfo Home { get; set; } = new();
+        public OddsInfo Draw { get; set; } = new();
+        public OddsInfo Away { get; set; } = new();
+    }
+
+    public class OddsInfo
+    {
+        public decimal? Odds { get; set; }
+        public string? Bookmaker { get; set; }
+    }
+
+    public class MarginInfo
+    {
+        public decimal Percent { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class ConsensusOdds
+    {
+        public decimal Home { get; set; }
+        public decimal Draw { get; set; }
+        public decimal Away { get; set; }
+    }
+
+    public class ValueBetsInfo
+    {
+        public List<string> Home { get; set; } = [];
+        public List<string> Draw { get; set; } = [];
+        public List<string> Away { get; set; } = [];
+        public bool HasAny => Home.Count != 0 || Draw.Count != 0 || Away.Count != 0;
+    }
+}
