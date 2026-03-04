583020211e324c2ce24b50a83708c7e4d9a86320
arunpun <83875939+arunpun@users.noreply.github.com>
2026-03-04 20:43:07 +0000

    Added TheOddService

---
+++ Services/TheOddsService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+using SportsOddsAnalyzer.Models;
+
+namespace SportsOddsAnalyzer.Services
+{
+    public class TheOddsService(HttpClient httpClient, IConfiguration config)
+    {
+        private readonly HttpClient _http = httpClient;
+        private readonly string? _apiKey = config["TheOddsKey"];
+
+        public async Task<List<Sport>> GetInSeasonSportsAsync()
+        {
+            string url = $"https://api.the-odds-api.com/v4/sports?apiKey=326babd30e8c2f8f6b0c12090e57ec55";
+
+            var response = await _http.GetFromJsonAsync<List<Sport>>(url)
+                ?? throw new HttpIOException(HttpRequestError.InvalidResponse, "Failed to retrieve in season sports");
+
+            return response;
+        }
+    }
+}
