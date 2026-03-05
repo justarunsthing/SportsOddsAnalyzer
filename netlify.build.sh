f895485cabafb58fc221cff9e1a1d077e4a7867f
justarunsthing <justarunsthing@outlook.com>
2026-03-05 10:41:01 +0000

    Added bash script

---
+++ netlify.build.sh
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env bash
+set -e
+
+# install latest .NET8.0 release
+pushd /tmp
+wget https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.sh
+
+# Make the script executable
+chmod u+x /tmp/dotnet-install.sh
+/tmp/dotnet-install.sh --channel 8.0
+
+# Return to project directory
+popd
+
+# Now we can publish (since we've installed .NET in this environment)
+dotnet publish -c Release -o release
